0ba1c614f16084997c217bd3de90bab1d0aa668a
Maxicids <maxicids@yandex.by>
2021-04-25 09:42:04 +0300

    Open connection

---
+++ Lab10/Lab10/DB/ConnectionProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Lab10.DB
+{
+    public class ConnectionProvider
+    {
+        private SqlConnection connection;
+
+        public SqlConnection GetConnection()
+        {
+            return connection ?? (connection =
+                new SqlConnection(ConfigurationManager.ConnectionStrings["MessengerContext"].ConnectionString));
+        }
+    }
+}
